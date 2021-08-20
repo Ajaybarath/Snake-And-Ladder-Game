c2f105279d13c9225f780544b7c18acdba7ad852
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 18:43:56 +0530

    ADDED: two players added to the game

--- src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
+++ src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
@@ -1,49 +1,98 @@
 package com.snakeAndLadderGame;
 
 public class SnakeAndLadder {
-	
+
 	public static void main(String args[]) {
-		
+
 		int player1Position = 0;
+		int player2Position = 0;
 		int numberOfDieRolls = 0;
-		
-		while (player1Position != 100) {
-			int playStatus =  (int) (Math.floor(Math.random()*10) % 3);
-			int dieValue = (int) (Math.floor(Math.random()*10) % 6) + 1;
-			
+
+		int playersTurn = 1;
+
+		while (player1Position != 100 && player2Position != 100) {
+
+			int playStatus = (int) (Math.floor(Math.random() * 10) % 3);
+			int dieValue = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+
 //			System.out.println("The die value is " + dieValue);
-			
+
 			if (playStatus == 0) {
-				System.out.println("Player remained at " + player1Position);
-			}
-			else if (playStatus == 1) {
-				player1Position += dieValue;
-				
-				if (player1Position > 100) {
-					player1Position -= dieValue;
-					System.out.println("Player remained at " + player1Position);
+				if (playersTurn == 1) {
+					System.out.println("Player1 remained at " + player1Position);
+					playersTurn = 2;
+					
 				}
 				else {
-					System.out.println("Player moved to " + player1Position);
+					System.out.println("Player2 remained at " + player2Position);
+					playersTurn = 1;
+
 				}
-				
-			}
-			else if (playStatus == 2) {
-				player1Position -= dieValue;
-				
-				if (player1Position < 0) {
-					player1Position = 0;
+			} else if (playStatus == 1) {
+
+				if (playersTurn == 1) {
+					player1Position += dieValue;
+
+					if (player1Position > 100) {
+						player1Position -= dieValue;
+						System.out.println("Player1 remained at " + player1Position);
+						playersTurn = 2;
+					} else {
+						System.out.println("Player1 moved to " + player1Position);
+					}
+
+				} else {
+					player2Position += dieValue;
+
+					if (player2Position > 100) {
+						player2Position -= dieValue;
+						System.out.println("Player2 remained at " + player2Position);
+						playersTurn = 1;
+					} else {
+						System.out.println("Player2 moved to " + player2Position);
+					}
+
 				}
-				
-				System.out.println("Player fell to " + player1Position);
+
+			} else if (playStatus == 2) {
+
+				if (playersTurn == 1) {
+					player1Position -= dieValue;
+
+					if (player1Position < 0) {
+						player1Position = 0;
+					}
+
+					System.out.println("Player1 fell to " + player1Position);
+					
+					playersTurn = 2;
+				} else {
+					player2Position -= dieValue;
+
+					if (player2Position < 0) {
+						player2Position = 0;
+					}
+
+					System.out.println("Player2 fell to " + player2Position);
+					
+					playersTurn = 1;
+				}
+
 			}
-			
+
 			numberOfDieRolls++;
-			
+
 		}
 		
+		if (player1Position == 100) {
+			System.out.println("Player 1 won the game");
+		}
+		else {
+			System.out.println("Player 2 won the game");
+		}
+
 		System.out.println("The dies are rolled " + numberOfDieRolls + " times");
-		
+
 	}
 
 }
