de7721be13b7603e03a351b8c125c3ba2a738477
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 18:14:35 +0530

    snake and ladder condition added

--- src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
+++ src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
@@ -3,12 +3,30 @@ package com.snakeAndLadderGame;
 public class SnakeAndLadder {
 	
 	public static void main(String args[]) {
-		int startPosition = 0;
-		int palyer1 = 0;
+		
+		int player1Position = 0;
 		
 		int dieValue = (int) (Math.floor(Math.random()*10) % 6) + 1;
 		
 		System.out.println("The die value is " + dieValue);
+		
+		while (player1Position <= 100) {
+			int playStatus =  (int) (Math.floor(Math.random()*10) % 3);
+			
+			if (playStatus == 0) {
+				System.out.println("Player remained at " + player1Position);
+			}
+			else if (playStatus == 1) {
+				player1Position += dieValue;
+				System.out.println("Player moved to " + player1Position);
+			}
+			else if (playStatus == 2) {
+				player1Position -= dieValue;
+				System.out.println("Player fell by " + player1Position);
+			}
+			
+			
+		}
 		
 	}
 
