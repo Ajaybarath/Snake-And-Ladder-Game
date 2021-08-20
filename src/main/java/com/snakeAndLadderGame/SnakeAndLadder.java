08ffcc52d3302c036505d5c40f2c1d7d08de1260
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 18:04:40 +0530

    die value generator added

--- src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
+++ src/main/java/com/snakeAndLadderGame/SnakeAndLadder.java
@@ -6,6 +6,10 @@ public class SnakeAndLadder {
 		int startPosition = 0;
 		int palyer1 = 0;
 		
+		int dieValue = (int) (Math.floor(Math.random()*10) % 6) + 1;
+		
+		System.out.println("The die value is " + dieValue);
+		
 	}
 
 }
